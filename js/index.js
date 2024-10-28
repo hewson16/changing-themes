da520d5819c2f54aa34dbfa474bea016fba26771
phewson16 <peterhewson16@gmail.com>
2024-10-28 18:46:49 -0400

    func works properly

--- js/index.js
+++ js/index.js
@@ -1,16 +1,10 @@
 function changeTheme() {
-    console.log('changeTheme func')
 
     const selectedTheme = document.querySelector('input[name="theme"]:checked');
-
-    
     
     if (selectedTheme) {
         const themeValue = selectedTheme.value;
-        console.log("Selected theme:", themeValue);
-
         document.body.className = themeValue; 
-    } else {
-        console.log("No theme selected.");
-    }
+    } 
+
 }
